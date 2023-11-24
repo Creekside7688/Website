4c14798299ff190a4fae270c5d8a9639afdaeb4f
Zylve <70604613+Zylve@users.noreply.github.com>
2023-11-24 13:10:15 -0800

    Fix path parser

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 function loadURLContent() {
     var currentPath = window.location.pathname;
-    document.getElementById('contentFrame').src = "https://team7688.weebly.com/" + currentPath;
+    console.log(currentPath);
+    document.getElementById('contentFrame').src = "https://team7688.weebly.com" + currentPath;
 }
 
 window.onload = loadURLContent;
