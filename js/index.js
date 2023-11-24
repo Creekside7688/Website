080c1ddc5be0b424d908c138a3ae241ba438f165
Zylve <70604613+Zylve@users.noreply.github.com>
2023-11-24 13:11:27 -0800

    Hide path in url

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function loadURLContent() {
     var currentPath = window.location.pathname;
     console.log(currentPath);
     document.getElementById('contentFrame').src = "https://team7688.weebly.com" + currentPath;
+    window.location.pathname = "/";
 }
 
 window.onload = loadURLContent;
