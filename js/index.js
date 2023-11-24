c64c8ab6a81a66a84037d0012fa1b43848d2361d
Zylve <70604613+Zylve@users.noreply.github.com>
2023-11-24 13:12:19 -0800

    wtf

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ function loadURLContent() {
     var currentPath = window.location.pathname;
     console.log(currentPath);
     document.getElementById('contentFrame').src = "https://team7688.weebly.com" + currentPath;
-    window.location.pathname = "/";
 }
 
 window.onload = loadURLContent;
