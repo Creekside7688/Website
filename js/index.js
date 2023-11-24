d5509e5ec8185e997477776e0d688251f041fe11
Zylve <70604613+Zylve@users.noreply.github.com>
2023-11-24 13:17:37 -0800

    remove path

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ function loadURLContent() {
     var currentPath = window.location.pathname;
     console.log(currentPath);
     document.getElementById('contentFrame').src = "https://team7688.weebly.com" + currentPath;
-    window.history.replaceState({}, document.title, window.location.hostname)
+    window.history.replaceState({}, document.title, "")
 }
 
 window.onload = loadURLContent;
