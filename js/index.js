8c1cfba71bc59159ab3a9b97be97ab0f2a889838
Zylve <70604613+Zylve@users.noreply.github.com>
2023-11-24 13:27:52 -0800

    Titles

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ function loadURLContent() {
     var currentPath = window.location.pathname;
     console.log(currentPath);
     document.getElementById('contentFrame').src = "https://team7688.weebly.com" + currentPath;
-    document.title = document.getElementById('contentFrame').contentWindow.document.title;
     window.history.replaceState({}, document.title, "/")
 }
 
