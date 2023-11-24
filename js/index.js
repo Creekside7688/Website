f359eae02425b72b61f373e9d2c95d851e381d0a
Zylve <70604613+Zylve@users.noreply.github.com>
2023-11-24 13:16:38 -0800

    Maybe this works

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function loadURLContent() {
     var currentPath = window.location.pathname;
     console.log(currentPath);
     document.getElementById('contentFrame').src = "https://team7688.weebly.com" + currentPath;
+    window.history.replaceState({}, document.title, window.location.hostname)
 }
 
 window.onload = loadURLContent;
