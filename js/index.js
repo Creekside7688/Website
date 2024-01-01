51c4f7be3ae6c55bcb0b949d0e7120c91c326d8c
Zylve <70604613+Zylve@users.noreply.github.com>
2024-01-01 15:12:54 -0800

    alert test

--- js/index.js
+++ js/index.js
@@ -2,8 +2,6 @@ function loadURLContent() {
     var currentPath = window.location.pathname;
     document.getElementById('contentFrame').src = "https://team7688.weebly.com" + currentPath;
     window.history.replaceState({}, document.title, "/")
-
-    console.log(document.getElementById('contentFrame').contentWindow.location.href);
 }
 
 window.onload = loadURLContent;
