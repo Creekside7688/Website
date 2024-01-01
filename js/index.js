ae71aab326a814f55e5f9a310c1ffbedb7c1d13d
Zylve <70604613+Zylve@users.noreply.github.com>
2024-01-01 15:15:00 -0800

    update

--- js/index.js
+++ js/index.js
@@ -4,4 +4,8 @@ function loadURLContent() {
     window.history.replaceState({}, document.title, "/")
 }
 
+function updatePath() {
+    console.log(document.getElementById('contentFrame').src);
+}
+
 window.onload = loadURLContent;
